6065d8515b01a2f54db83eb53c5c554e9bf83212
Higor Turetta <69050147+HigorTuretta@users.noreply.github.com>
2022-11-03 12:49:00 -0300

    Result Text Animation Add

--- calcResult.js
+++ calcResult.js
@@ -12,9 +12,18 @@ export function calcResult(timeDiff) {
           : " diária(s) e meia"
         : " diária(s)"
     } `;
+    addEntrance(result);
   } else {
     result.innerHTML = `Você tem Direito a: ${
       hoursDiff > 12 ? 1 + " diária" : "meia diária"
     }`;
+    addEntrance(result);
   }
+}
+
+function addEntrance(element) {
+  element.classList.add("scale-in-center");
+  setTimeout(() => {
+    element.classList.remove("scale-in-center");
+  }, 1000);
 }
