917e5783cfea40648209890a2ce04fc8b161594d
Higor Turetta <69050147+HigorTuretta@users.noreply.github.com>
2022-11-03 12:15:00 -0300

    Touch Bug Fix

--- calcResult.js
+++ calcResult.js
@@ -1,21 +1,4 @@
-import { getFormattedTime, getFormattedDate } from "./app/utils/formatDates";
-import datetimeDifference from "datetime-difference";
-
-export function getDates(ev) {
-  ev.preventDefault();
-  const hora1 = getFormattedTime(new Date(`2022-01-01 ${horaIda.value}`));
-  const hora2 = getFormattedTime(new Date(`2022-01-01 ${horaVolta.value}`));
-
-  const date1 = getFormattedDate(new Date(dataIda.value));
-  const date2 = getFormattedDate(new Date(dataVolta.value));
-
-  const dataHoraIda = new Date(date1 + " " + hora1);
-  const dataHoraVolta = new Date(date2 + " " + hora2);
-
-  calcResult(datetimeDifference(dataHoraVolta, dataHoraIda));
-}
-
-function calcResult(timeDiff) {
+export function calcResult(timeDiff) {
   const result = document.getElementById("result-area");
   const hoursDiff = timeDiff.hours;
   const daysDiff = timeDiff.days;
@@ -29,5 +12,9 @@ function calcResult(timeDiff) {
           : " diária(s) e meia"
         : " diária(s)"
     } `;
+  } else {
+    result.innerHTML = `Você tem Direito a: ${
+      hoursDiff > 12 ? 1 + " diária" : "meia diária"
+    }`;
   }
 }
--- main.js
+++ main.js
@@ -3,8 +3,9 @@ import "./app/styles/header.css";
 import "./app/styles/inputs.css";
 import "./app/styles/buttons.css";
 
-import { getDates } from "./calcResult";
-import {getCurrentDate} from "./app/utils/formatDates"
+import { calcResult } from "./calcResult";
+import {getCurrentDate, getFormattedTime, getFormattedDate } from "./app/utils/formatDates"
+import datetimeDifference from "datetime-difference";
 
 const btn = document.getElementById("calc");
 let dataIda = document.getElementById("dataIda");
@@ -18,5 +19,20 @@ window.onload = () => {
   dataVolta.value = getCurrentDate();
   horaVolta.value = "08:00";
 
-  btn.addEventListener("click", getDates(event), false);
+  btn.addEventListener("click", getDates);
+  btn.addEventListener("touchstart", getDates);
 };
+
+export function getDates() {
+
+  const hora1 = getFormattedTime(new Date(`2022-01-01 ${horaIda.value}`));
+  const hora2 = getFormattedTime(new Date(`2022-01-01 ${horaVolta.value}`));
+
+  const date1 = getFormattedDate(new Date(dataIda.value));
+  const date2 = getFormattedDate(new Date(dataVolta.value));
+
+  const dataHoraIda = new Date(date1 + " " + hora1);
+  const dataHoraVolta = new Date(date2 + " " + hora2);
+
+  calcResult(datetimeDifference(dataHoraVolta, dataHoraIda));
+}
