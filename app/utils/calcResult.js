43fed3bc1df79b9a3f7c92bd537335f6d7de629d
Higor Turetta <69050147+HigorTuretta@users.noreply.github.com>
2022-11-03 10:33:31 -0300

    Result Style Update and Mobile Style Add

--- app/utils/calcResult.js
+++ app/utils/calcResult.js
@@ -1,13 +1,10 @@
-
 export function calcResult(timeDiff) {
-    const hoursDiff = timeDiff.hours;
-    const daysDiff = timeDiff.days;
-    const minutesDiff = timeDiff.minutes;
-  
-    console.log(daysDiff);
-    if (daysDiff >= 1) {
-      result.innerHTML = `Total: ${hoursDiff > 12 ? daysDiff + 1 : daysDiff}${
-        hoursDiff <= 12 ? " diária(s) e meia" : " diária(s)"
-      } `;
-    }
+  const result = document.getElementById("result-area");
+  const hoursDiff = timeDiff.hours;
+  const daysDiff = timeDiff.days;
+  if (daysDiff >= 1) {
+    result.innerHTML = `Você tem Direito a: ${
+      hoursDiff > 12 ? daysDiff + 1 : daysDiff
+    }${hoursDiff <= 12 ? hoursDiff == 0 ? " diária(s)" :" diária(s) e meia" : " diária(s)"} `;
   }
+}
