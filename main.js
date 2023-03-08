ad6ee72e450095962d22132fbf878e380afbec0e
Higor Turetta <69050147+HigorTuretta@users.noreply.github.com>
2023-03-07 21:34:09 -0300

    Verificação de Datas adicionado

--- main.js
+++ main.js
@@ -35,5 +35,54 @@ export function getDates() {
   const dataHoraIda = new Date(date1 + " " + hora1);
   const dataHoraVolta = new Date(date2 + " " + hora2);
 
+
+// Exibe a mensagem de erro caso a data de retorno seja anterior a data de ida
+if (dataHoraVolta < dataHoraIda) {
+  showErrorPopup("A data de retorno não pode ser anterior a data de ida, gentileza verificar.");
+  return;
+}
+
   calcResult(datetimeDifference(dataHoraVolta, dataHoraIda));
+}
+
+function showErrorPopup(message) {
+  // Cria a div da popup
+  const popup = document.createElement("div");
+  popup.classList.add("popup_popUp");
+
+  // Cria o conteúdo da popup
+  const content = document.createElement("div");
+  content.classList.add("content_popUp");
+
+  const messageEl = document.createElement("div");
+  messageEl.classList.add("message_popUp");
+  messageEl.innerText = message;
+  content.appendChild(messageEl);
+
+  // Adiciona o conteúdo à popup
+  popup.appendChild(content);
+
+  // Cria o botão de fechar
+  const closeBtn = document.createElement("button");
+  closeBtn.classList.add("btn_popUp");
+  closeBtn.addEventListener("click", () => {
+    content.classList.add("hide_popUp");
+    setTimeout(() => {
+      document.body.removeChild(popup);
+    }, 300);
+  });
+
+  // Adiciona o botão ao conteúdo
+  content.appendChild(closeBtn);
+
+  // Adiciona a popup ao body
+  document.body.appendChild(popup);
+
+  // Adiciona a animação de entrada da popup
+  setTimeout(() => {
+    popup.classList.add("show_popUp");
+  }, 0);
+  setTimeout(() => {
+    content.classList.add("show_popUp");
+  }, 0);
 }
